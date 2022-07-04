5cb36844da533ed8592e6135a8c75325624f92cb
Rowan Quinn <35435338+rchase5@users.noreply.github.com>
2022-07-04 00:49:00 -0400

    Added missing API salt

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'QSslc2d5fjR4ZmosMG4uMnVGbC57SlZqVm5JODFTQ050IztfYjlvQzh8d1Yrcy80Kn1HSHo2YysvdVNFeE00YQ'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'UVNzbGMyZDVmalI0Wm1vc01HNHVNblZHYkM1N1NsWnFWbTVKT0'),
   },
 });
