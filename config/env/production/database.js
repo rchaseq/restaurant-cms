9ab3d55dcb191887b554fea459f9ff018740b8b0
Rowan Quinn <35435338+rchaseq@users.noreply.github.com>
2022-07-18 16:18:21 -0400

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
-      connection: {
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
